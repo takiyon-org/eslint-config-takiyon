c746c71018369b4ec236f0a6f7912e665b360c20
Jake Zatecky <jake.zatecky@gmail.com>
2017-09-25 18:01:22 -0400

    Temporary `function-paren-newline` fix until a more permanent solutions arrives
    Unfortunately, `consistent` is insufficient and should be swapped as quickly as possible to prevent unacceptable styles

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ module.exports = {
         'class-methods-use-this': 'off',
         indent: ['error', 4, {
             SwitchCase: 1,
-        }]
+        }],
+        // Temporarily switch to consistent until an option exists to support multiline with
+        // single arguments
+        // https://github.com/eslint/eslint/issues/9286
+        'function-paren-newline': ['error', 'consistent'],
     },
 };
