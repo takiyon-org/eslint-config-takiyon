9a74bd42996e66a2b74a45e67b213b19aadf0087
Jake Zatecky <jake.zatecky@gmail.com>
2018-06-26 20:23:41 -0400

    Make operator-linebreak require operators at the end of a line

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ module.exports = {
     parser: 'babel-eslint',
     rules: {
         'class-methods-use-this': 'off',
-        indent: ['error', 4, {
-            SwitchCase: 1,
-        }],
         // Temporarily switch to consistent until an option exists to support multiline with
         // single arguments
         // https://github.com/eslint/eslint/issues/9286
         'function-paren-newline': ['error', 'consistent'],
+        indent: ['error', 4, {
+            SwitchCase: 1,
+        }],
+        'operator-linebreak': ['error', 'after']
     },
 };
