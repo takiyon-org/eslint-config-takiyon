8f76df47b44ac6a53c5729d16fe3865c134479fb
Jake Zatecky <jake.zatecky@gmail.com>
2022-09-21 11:41:42 -0400

    Adopt `multiline-arguments` option for `function-paren-newline`
    Also add explainers for each rule change.

--- index.js
+++ index.js
@@ -2,14 +2,18 @@ module.exports = {
     extends: 'airbnb-base',
     parser: '@babel/eslint-parser',
     rules: {
+        // Do not coerce methods into static state simply because they lack `this`
         'class-methods-use-this': 'off',
-        // Temporarily switch to consistent until an option exists to support multiline with
-        // single arguments
-        // https://github.com/eslint/eslint/issues/9286
-        'function-paren-newline': ['error', 'consistent'],
+
+        // Allow multiline arguments, but require newlines between each one in that case
+        'function-paren-newline': ['error', 'multiline-arguments'],
+
+        // Two spaces are not enough; switch statements should have indentation
         indent: ['error', 4, {
             SwitchCase: 1,
         }],
+
+        // If using multi-line operators, require a linebreak after for readability
         'operator-linebreak': ['error', 'after']
     },
 };
